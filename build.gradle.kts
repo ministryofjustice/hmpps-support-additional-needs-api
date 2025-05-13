a3e24a8cf6dd2ba649e206a3afbe7a57ae7e43ff
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2025-05-13 13:25:24 +0100

    RR-1359 - Basic RDS config (#3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,44 @@
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.1.0"
   kotlin("plugin.spring") version "2.1.20"
+  kotlin("plugin.jpa") version "2.1.20"
+
+  `java-test-fixtures`
+}
+
+allOpen {
+  annotations(
+    "javax.persistence.Entity",
+    "javax.persistence.MappedSuperclass",
+    "javax.persistence.Embeddable",
+  )
 }
 
 configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
+val postgresqlVersion = "42.7.5"
+val testContainersVersion = "1.21.0"
+val buildDirectory: Directory = layout.buildDirectory.get()
+
 dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.3")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
 
+  runtimeOnly("org.flywaydb:flyway-database-postgresql")
+  runtimeOnly("org.postgresql:postgresql:$postgresqlVersion")
+
+  // Test dependencies
   testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.3")
   testImplementation("org.wiremock:wiremock-standalone:3.13.0")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.26") {
     exclude(group = "io.swagger.core.v3")
   }
+  testImplementation("org.testcontainers:postgresql:$testContainersVersion")
+  testImplementation("org.testcontainers:localstack:$testContainersVersion")
 }
 
 kotlin {
@@ -26,5 +48,32 @@ kotlin {
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     compilerOptions.jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
+  }
+}
+
+/*
+  `dps-gradle-spring-boot` disabled the jar task in jira DT-2070, specifically to prevent the generation of the
+  Spring Boot plain jar. The reason was that the `Dockerfile` copies the Spring Boot fat jar with:
+  `COPY --from=builder --chown=appuser:appgroup /app/build/libs/hmpps-education-and-work-plan-api*.jar /app/app.jar`
+  The fat jar includes the date in the filename, hence needing to use a wildcard. Using the wildcard causes problems
+  if there are multiple matching files (eg: the plain jar)
+
+  The plugin `java-test-fixtures` requires the plain jar in order to access the compiled classes from the `main` source
+  root. The `jar` task has been re-enabled here to allow `java-test-fixtures` to see the `main` classes, and a hook
+  has been added to the `assemble` task to remove the plain jar and test-fixtures jar before assembling the Spring Boot
+  fat jar.
+ */
+tasks.named("jar") {
+  enabled = true
+}
+tasks.named("assemble") {
+  // `assemble` task assembles the classes and dependencies into a fat jar
+  // Beforehand we need to remove the plain jar and test-fixtures jars if they exist
+  doFirst {
+    delete(
+      fileTree(buildDirectory)
+        .include("libs/*-plain.jar")
+        .include("libs/*-test-fixtures.jar"),
+    )
   }
 }
