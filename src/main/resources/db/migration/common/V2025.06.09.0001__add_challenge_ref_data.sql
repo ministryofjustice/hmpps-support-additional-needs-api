a4ac0cb3eed8d1f33fd8c0d258b9e5a067cc1517
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2025-06-09 14:27:02 +0100

    initial checkin for challenge api. (#20)

---
+++ src/main/resources/db/migration/common/V2025.06.09.0001__add_challenge_ref_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO reference_data (id, domain, code, description, list_sequence)
+VALUES
+    (gen_random_uuid(), 'CHALLENGE', 'ATTN_ORG_TIME', 'Attention, Organising & Time Management', 1),
+    (gen_random_uuid(), 'CHALLENGE', 'LITERACY_SKILLS', 'Literacy Skills', 2),
+    (gen_random_uuid(), 'CHALLENGE', 'NUMERACY_SKILLS', 'Numeracy Skills', 3),
+    (gen_random_uuid(), 'CHALLENGE', 'MEMORY', 'Memory', 4),
+    (gen_random_uuid(), 'CHALLENGE', 'PROCESSING_SPEED', 'Processing Speed', 5),
+    (gen_random_uuid(), 'CHALLENGE', 'LANG_COMM_SKILLS', 'Language & Communication Skills', 6),
+    (gen_random_uuid(), 'CHALLENGE', 'PHYSICAL_SKILLS', 'Physical Skills & Coordination', 7),
+    (gen_random_uuid(), 'CHALLENGE', 'SENSORY', 'Sensory', 8),
+    (gen_random_uuid(), 'CHALLENGE', 'EMOTIONS_FEELINGS', 'Emotions & Feelings', 9);
