a0db6135bcc75e2cc1c714d4d0700974de7a7586
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2025-06-10 15:08:08 +0100

    Implementing create and get plan endpoints.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/resource/ConditionController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/resource/ConditionController.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.supportadditionalneedsapi.resource
 
-import jakarta.transaction.Transactional
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.security.access.prepost.PreAuthorize
@@ -21,7 +20,6 @@ import java.util.*
 
 @RestController
 @RequestMapping("/profile/{prisonNumber}/conditions")
-@Transactional
 class ConditionController(private val conditionService: ConditionService) {
   @PreAuthorize(HAS_EDIT_ELSP)
   @PostMapping
