752a76140eaadb9adc844d8270667f92246d3c64
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2025-07-02 16:59:14 +0100

    endpoint to allow for test data creation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/resource/TestDataController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/resource/TestDataController.kt
@@ -4,10 +4,8 @@ import jakarta.validation.Valid
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.http.HttpStatus
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -15,13 +13,6 @@ import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.supportadditionalneedsapi.domain.entity.PlanCreationScheduleEntity
 import uk.gov.justice.digital.hmpps.supportadditionalneedsapi.domain.entity.PlanCreationScheduleStatus
 import uk.gov.justice.digital.hmpps.supportadditionalneedsapi.domain.repository.PlanCreationScheduleRepository
-import uk.gov.justice.digital.hmpps.supportadditionalneedsapi.resource.model.ChallengeListResponse
-import uk.gov.justice.digital.hmpps.supportadditionalneedsapi.resource.model.ChallengeResponse
-import uk.gov.justice.digital.hmpps.supportadditionalneedsapi.resource.model.CreateChallengesRequest
-import uk.gov.justice.digital.hmpps.supportadditionalneedsapi.resource.model.UpdateChallengeRequest
-import uk.gov.justice.digital.hmpps.supportadditionalneedsapi.service.ChallengeService
-import uk.gov.justice.digital.hmpps.supportadditionalneedsapi.service.PlanCreationScheduleService
-import uk.gov.justice.digital.hmpps.supportadditionalneedsapi.service.ReviewScheduleService
 import java.time.LocalDate
 import java.util.*
 
@@ -36,16 +27,15 @@ class TestDataController(private val planCreationScheduleRepository: PlanCreatio
     @PathVariable prisonNumber: String,
     @Valid
     @RequestBody request: Request,
-  ): PlanCreationScheduleEntity =
-    planCreationScheduleRepository.save(
-      PlanCreationScheduleEntity(
-        prisonNumber = prisonNumber,
-        deadlineDate = request.deadlineDate,
-        status = PlanCreationScheduleStatus.SCHEDULED,
-        createdAtPrison = prisonNumber,
-        updatedAtPrison = prisonNumber,
-      ),
-    )
+  ): PlanCreationScheduleEntity = planCreationScheduleRepository.save(
+    PlanCreationScheduleEntity(
+      prisonNumber = prisonNumber,
+      deadlineDate = request.deadlineDate,
+      status = PlanCreationScheduleStatus.SCHEDULED,
+      createdAtPrison = request.prisonId,
+      updatedAtPrison = request.prisonId,
+    ),
+  )
 }
 
-data class Request(val deadlineDate: LocalDate, val prisonNumber: String)
+data class Request(val deadlineDate: LocalDate, val prisonId: String)
