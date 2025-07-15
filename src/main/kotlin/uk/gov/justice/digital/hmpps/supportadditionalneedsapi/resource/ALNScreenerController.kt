1666c5fb3564991b0d6eb3324d0aac8860c1bd43
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2025-07-15 09:32:38 +0100

    ALN screener endpoint spec

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/resource/ALNScreenerController.kt
@@ -0,0 +1,31 @@
+package uk.gov.justice.digital.hmpps.supportadditionalneedsapi.resource
+
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.supportadditionalneedsapi.resource.model.ALNScreenerRequest
+import uk.gov.justice.digital.hmpps.supportadditionalneedsapi.service.ALNScreenerService
+import java.util.*
+
+@RestController
+@RequestMapping("/profile/{prisonNumber}/aln-screener")
+class ALNScreenerController(private val alnScreenerService: ALNScreenerService) {
+  @PreAuthorize(HAS_EDIT_ELSP)
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  fun createALNScreener(
+    @PathVariable prisonNumber: String,
+    @Valid
+    @RequestBody request: ALNScreenerRequest,
+  ): ResponseEntity<Void> {
+    alnScreenerService.createScreener(prisonNumber, request)
+    return ResponseEntity.status(HttpStatus.CREATED).build()
+  }
+}
