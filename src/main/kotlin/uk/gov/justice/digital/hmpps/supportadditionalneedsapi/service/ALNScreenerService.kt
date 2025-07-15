f2e9dca1e1a84194ac7e17309cdb2be05f2e349c
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2025-07-15 10:42:40 +0100

    ALN screener endpoint spec (#109)
    * ALN screener endpoint spec
    
    * ALN screener endpoint spec
    
    * ALN screener endpoint spec
    
    * ALN screener endpoint spec

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/service/ALNScreenerService.kt
@@ -0,0 +1,10 @@
+package uk.gov.justice.digital.hmpps.supportadditionalneedsapi.service
+
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.supportadditionalneedsapi.resource.model.ALNScreenerRequest
+
+@Service
+class ALNScreenerService {
+  fun createScreener(prisonNumber: String, request: ALNScreenerRequest) {
+  }
+}
