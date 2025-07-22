b6b812dda12462e32ffc20d4988ce17383e1ac67
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2025-07-22 16:18:13 +0100

    added support strategy ref data categories (#123)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/integration/resource/GetReferenceDataCategoryTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/integration/resource/GetReferenceDataCategoryTest.kt
@@ -65,4 +65,31 @@ class GetReferenceDataCategoryTest : IntegrationTestBase() {
 
     assertThat(actualCategoryCodes).containsExactlyElementsOf(expectedCategoryCodes)
   }
+
+  @Test
+  fun `should return a list of SUPPORT_STRATEGY category reference data`() {
+    // Given
+    stubGetTokenFromHmppsAuth()
+
+    // When
+    val response = webTestClient.get()
+      .uri(URI_TEMPLATE, Domain.SUPPORT_STRATEGY)
+      .headers(setAuthorisation(roles = listOf("ROLE_SUPPORT_ADDITIONAL_NEEDS__ELSP__RW"), username = "testuser"))
+      .exchange()
+      .expectStatus()
+      .isOk
+      .returnResult(ReferenceDataListResponse::class.java)
+
+    // Then
+    val actual = response.responseBody.blockFirst()
+    assertThat(actual).isNotNull()
+    assertThat(actual!!.referenceDataList.size).isEqualTo(10)
+
+    // Ensure no duplicate codes
+    val uniqueCodes = actual.referenceDataList.map { it.code }.toSet()
+    assertThat(uniqueCodes.size).isEqualTo(actual.referenceDataList.size)
+
+    // Ensure all entries have non-null descriptions
+    assertThat(actual.referenceDataList).allMatch { it.description != null }
+  }
 }
