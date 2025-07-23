d87310911f6db5be5d9ee4ddc67dd656a1d64221
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2025-07-23 10:45:55 +0100

    added indexes on reference columns

---
+++ src/main/resources/db/migration/common/V2025.07.23.0002__indexes_on_reference_columns.sql
@@ -0,0 +1,12 @@
+
+create index challenge_reference_idx
+    on challenge (reference);
+
+create index strength_reference_idx
+    on strength (reference);
+
+create index support_strategy_reference_idx
+    on support_strategy (reference);
+
+create index condition_reference_idx
+    on condition (reference);
