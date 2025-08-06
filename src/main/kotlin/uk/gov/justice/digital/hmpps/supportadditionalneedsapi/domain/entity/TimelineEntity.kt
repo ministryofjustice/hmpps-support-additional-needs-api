779fb803cfee0aa42f5626b02e0c2ccab0453fab
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2025-08-06 15:55:22 +0100

    initial commit of timeline code (#148)
    * initial commit of timeline code
    
    * initial commit of timeline code
    
    * initial commit of timeline code

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/domain/entity/TimelineEntity.kt
@@ -0,0 +1,55 @@
+package uk.gov.justice.digital.hmpps.supportadditionalneedsapi.domain.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.hibernate.annotations.CreationTimestamp
+import org.springframework.data.annotation.CreatedBy
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.Instant
+import java.util.*
+
+@Entity
+@EntityListeners(value = [AuditingEntityListener::class])
+@Table(name = "timeline")
+data class TimelineEntity(
+
+  @Id
+  val id: UUID = UUID.randomUUID(),
+
+  @Column(updatable = false)
+  val prisonNumber: String,
+
+  @Enumerated(EnumType.STRING)
+  var event: EventType,
+
+  var additionalInfo: String? = null,
+
+  @Column(updatable = false)
+  val createdAtPrison: String,
+
+  @CreatedBy
+  @Column(updatable = false)
+  var createdBy: String? = null,
+
+  @CreationTimestamp
+  @Column(updatable = false)
+  var createdAt: Instant? = null,
+
+)
+
+enum class EventType {
+  CONDITION_ADDED,
+  CHALLENGE_ADDED,
+  STRENGTH_ADDED,
+  SUPPORT_STRATERGY_ADDED,
+  CURIOUS_ASSESSMENT_TRIGGER,
+  CURIOUS_EDUCATION_TRIGGER,
+  ELSP_CREATED,
+  ELSP_UPDATED,
+  ALN_ASSESSMENT_ADDED,
+}
---
+++ src/main/resources/db/migration/common/V2025.08.06.0002__Prisoner_timeline.sql
@@ -0,0 +1,9 @@
+CREATE TABLE timeline (
+          id                    UUID PRIMARY KEY,
+          prison_number         VARCHAR NOT NULL,
+          event                 VARCHAR NOT NULL,
+          additional_info       TEXT,
+          created_at            TIMESTAMP NOT NULL,
+          created_by            VARCHAR(50) NOT NULL,
+          created_at_prison     VARCHAR(3) NOT NULL
+);
