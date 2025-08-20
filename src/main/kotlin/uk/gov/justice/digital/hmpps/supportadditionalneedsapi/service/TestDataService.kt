24a1ade1542373fe86831a9bd4d068ec8767bf8f
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2025-08-20 12:07:58 +0100

    added trigger endpoints (#174)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/service/TestDataService.kt
@@ -0,0 +1,33 @@
+package uk.gov.justice.digital.hmpps.supportadditionalneedsapi.service
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.stereotype.Service
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest
+import software.amazon.awssdk.services.sqs.model.SendMessageResponse
+import uk.gov.justice.digital.hmpps.supportadditionalneedsapi.messaging.SqsMessage
+import uk.gov.justice.hmpps.sqs.HmppsQueueService
+import uk.gov.justice.hmpps.sqs.MissingQueueException
+
+@Service
+class TestDataService(
+  private val objectMapper: ObjectMapper,
+  private val hmppsQueueService: HmppsQueueService,
+) {
+
+  val domainEventQueue by lazy {
+    hmppsQueueService.findByQueueId("supportadditionalneeds")
+      ?: throw MissingQueueException("HmppsQueue supportadditionalneeds not found")
+  }
+  val domainEventQueueClient by lazy { domainEventQueue.sqsClient }
+
+  fun sendDomainEvent(
+    message: SqsMessage,
+    queueUrl: String = domainEventQueue.queueUrl,
+  ): SendMessageResponse = domainEventQueueClient.sendMessage(
+    SendMessageRequest.builder()
+      .queueUrl(queueUrl)
+      .messageBody(
+        objectMapper.writeValueAsString(message),
+      ).build(),
+  ).get()
+}
