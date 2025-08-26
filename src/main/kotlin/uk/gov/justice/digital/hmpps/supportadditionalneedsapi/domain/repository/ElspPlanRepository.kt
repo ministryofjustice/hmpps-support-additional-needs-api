29d5aa5c036f670b8e00db77d904d7a1cf7e6a08
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2025-08-26 15:56:45 +0100

    check that the review history is recorded and retrievable

--- src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/domain/repository/ElspPlanRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/domain/repository/ElspPlanRepository.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.supportadditionalneedsapi.domain.repository
 
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.supportadditionalneedsapi.domain.entity.ElspPlanEntity
@@ -7,6 +8,7 @@ import java.util.UUID
 
 @Repository
 interface ElspPlanRepository : JpaRepository<ElspPlanEntity, UUID> {
+  @EntityGraph(attributePaths = ["otherContributors"])
   fun findByPrisonNumber(prisonNumber: String): ElspPlanEntity?
   fun existsByPrisonNumber(prisonNumber: String): Boolean
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/domain/repository/ElspReviewHistoryRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/domain/repository/ElspReviewHistoryRepository.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.supportadditionalneedsapi.domain.repository
 
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.supportadditionalneedsapi.domain.entity.ElspReviewHistoryEntity
@@ -7,5 +8,6 @@ import java.util.UUID
 
 @Repository
 interface ElspReviewHistoryRepository : JpaRepository<ElspReviewHistoryEntity, UUID> {
+  @EntityGraph(attributePaths = ["otherContributors"])
   fun findAllByPrisonNumber(prisonNumber: String): List<ElspReviewHistoryEntity>
 }
