eda8a510e4a1ac2d9b9e3522e5c333c9cb89d45d
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2025-10-20 16:07:56 +0100

    fix default value (#243)
    * fix default value
    
    * fix default value

--- src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/config/ReviewConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/config/ReviewConfig.kt
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration
 
 @ConfigurationProperties(prefix = "review")
 data class ReviewConfig(
-  var reviewDeadlineDaysToAdd: Long = 5,
+  var reviewDeadlineDaysToAdd: Long,
 )
 
 @Configuration
