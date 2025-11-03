7cd0a60f1311cde314dd3a978d11ed0eec145cb5
ayo.adedeji <ayo.adedeji@justice.gov.uk>
2025-11-03 10:51:50 +0000

    format

--- src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/config/PrimaryDataSourceConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/config/PrimaryDataSourceConfig.kt
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
 import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
@@ -19,61 +18,57 @@ import javax.sql.DataSource
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(
-    entityManagerFactoryRef = "primaryEntityManagerFactory",
-    transactionManagerRef = "primaryTransactionManager",
-    basePackages = ["uk.gov.justice.digital.hmpps.supportadditionalneedsapi.domain.repository"],
-    excludeFilters = [
-        org.springframework.context.annotation.ComponentScan.Filter(
-            type = org.springframework.context.annotation.FilterType.REGEX,
-            pattern = ["uk\\.gov\\.justice\\.digital\\.hmpps\\.supportadditionalneedsapi\\.domain\\.repository\\.reporting\\..*"]
-        )
-    ]
+  entityManagerFactoryRef = "primaryEntityManagerFactory",
+  transactionManagerRef = "primaryTransactionManager",
+  basePackages = ["uk.gov.justice.digital.hmpps.supportadditionalneedsapi.domain.repository"],
+  excludeFilters = [
+    org.springframework.context.annotation.ComponentScan.Filter(
+      type = org.springframework.context.annotation.FilterType.REGEX,
+      pattern = ["uk\\.gov\\.justice\\.digital\\.hmpps\\.supportadditionalneedsapi\\.domain\\.repository\\.reporting\\..*"],
+    ),
+  ],
 )
 class PrimaryDataSourceConfig {
 
-    @Bean
-    @Primary
-    @ConfigurationProperties("spring.datasource-primary")
-    fun primaryDataSourceProperties(): DataSourceProperties = DataSourceProperties()
+  @Bean
+  @Primary
+  @ConfigurationProperties("spring.datasource-primary")
+  fun primaryDataSourceProperties(): DataSourceProperties = DataSourceProperties()
 
-    @Bean(name = ["primaryDataSource"])
-    @Primary
-    @ConfigurationProperties("spring.datasource-primary.hikari")
-    fun primaryDataSource(
-        @Qualifier("primaryDataSourceProperties") dataSourceProperties: DataSourceProperties
-    ): DataSource = dataSourceProperties
-        .initializeDataSourceBuilder()
-        .build()
+  @Bean(name = ["primaryDataSource"])
+  @Primary
+  @ConfigurationProperties("spring.datasource-primary.hikari")
+  fun primaryDataSource(
+    @Qualifier("primaryDataSourceProperties") dataSourceProperties: DataSourceProperties,
+  ): DataSource = dataSourceProperties
+    .initializeDataSourceBuilder()
+    .build()
 
-    @Bean
-    @Primary
-    @ConfigurationProperties("spring.jpa")
-    fun primaryJpaProperties(): JpaProperties = JpaProperties()
+  @Bean
+  @Primary
+  @ConfigurationProperties("spring.jpa")
+  fun primaryJpaProperties(): JpaProperties = JpaProperties()
 
-    @Bean(name = ["primaryEntityManagerFactory"])
-    @Primary
-    fun primaryEntityManagerFactory(
-        @Qualifier("primaryDataSource") dataSource: DataSource,
-        @Qualifier("primaryJpaProperties") jpaProperties: JpaProperties
-    ): LocalContainerEntityManagerFactoryBean {
-        return LocalContainerEntityManagerFactoryBean().apply {
-            setDataSource(dataSource)
-            setPersistenceProviderClass(HibernatePersistenceProvider::class.java)
-            persistenceUnitName = "primary"
-            setPackagesToScan("uk.gov.justice.digital.hmpps.supportadditionalneedsapi.domain.entity")
-            
-            val properties = HashMap<String, Any?>()
-            properties.putAll(jpaProperties.properties)
-            properties["hibernate.dialect"] = "org.hibernate.dialect.PostgreSQLDialect"
-            setJpaPropertyMap(properties)
-        }
-    }
+  @Bean(name = ["primaryEntityManagerFactory"])
+  @Primary
+  fun primaryEntityManagerFactory(
+    @Qualifier("primaryDataSource") dataSource: DataSource,
+    @Qualifier("primaryJpaProperties") jpaProperties: JpaProperties,
+  ): LocalContainerEntityManagerFactoryBean = LocalContainerEntityManagerFactoryBean().apply {
+    setDataSource(dataSource)
+    setPersistenceProviderClass(HibernatePersistenceProvider::class.java)
+    persistenceUnitName = "primary"
+    setPackagesToScan("uk.gov.justice.digital.hmpps.supportadditionalneedsapi.domain.entity")
 
-    @Bean(name = ["primaryTransactionManager"])
-    @Primary
-    fun primaryTransactionManager(
-        @Qualifier("primaryEntityManagerFactory") entityManagerFactory: LocalContainerEntityManagerFactoryBean
-    ): PlatformTransactionManager {
-        return JpaTransactionManager(entityManagerFactory.getObject()!!)
-    }
+    val properties = HashMap<String, Any?>()
+    properties.putAll(jpaProperties.properties)
+    properties["hibernate.dialect"] = "org.hibernate.dialect.PostgreSQLDialect"
+    setJpaPropertyMap(properties)
+  }
+
+  @Bean(name = ["primaryTransactionManager"])
+  @Primary
+  fun primaryTransactionManager(
+    @Qualifier("primaryEntityManagerFactory") entityManagerFactory: LocalContainerEntityManagerFactoryBean,
+  ): PlatformTransactionManager = JpaTransactionManager(entityManagerFactory.getObject()!!)
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/config/ReplicaDataSourceConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/config/ReplicaDataSourceConfig.kt
@@ -16,57 +16,53 @@ import javax.sql.DataSource
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories(
-    entityManagerFactoryRef = "replicaEntityManagerFactory",
-    transactionManagerRef = "replicaTransactionManager",
-    basePackages = ["uk.gov.justice.digital.hmpps.supportadditionalneedsapi.domain.repository.reporting"]
+  entityManagerFactoryRef = "replicaEntityManagerFactory",
+  transactionManagerRef = "replicaTransactionManager",
+  basePackages = ["uk.gov.justice.digital.hmpps.supportadditionalneedsapi.domain.repository.reporting"],
 )
 class ReplicaDataSourceConfig {
 
-    @Bean
-    @ConfigurationProperties("spring.datasource-replica")
-    fun replicaDataSourceProperties(): DataSourceProperties = DataSourceProperties()
+  @Bean
+  @ConfigurationProperties("spring.datasource-replica")
+  fun replicaDataSourceProperties(): DataSourceProperties = DataSourceProperties()
 
-    @Bean(name = ["replicaDataSource"])
-    @ConfigurationProperties("spring.datasource-replica.hikari")
-    fun replicaDataSource(
-        @Qualifier("replicaDataSourceProperties") dataSourceProperties: DataSourceProperties
-    ): DataSource = dataSourceProperties
-        .initializeDataSourceBuilder()
-        .build()
+  @Bean(name = ["replicaDataSource"])
+  @ConfigurationProperties("spring.datasource-replica.hikari")
+  fun replicaDataSource(
+    @Qualifier("replicaDataSourceProperties") dataSourceProperties: DataSourceProperties,
+  ): DataSource = dataSourceProperties
+    .initializeDataSourceBuilder()
+    .build()
 
-    @Bean(name = ["replicaEntityManagerFactory"])
-    fun replicaEntityManagerFactory(
-        @Qualifier("replicaDataSource") dataSource: DataSource
-    ): LocalContainerEntityManagerFactoryBean {
-        return LocalContainerEntityManagerFactoryBean().apply {
-            setDataSource(dataSource)
-            setPersistenceProviderClass(HibernatePersistenceProvider::class.java)
-            persistenceUnitName = "replica"
-            setPackagesToScan("uk.gov.justice.digital.hmpps.supportadditionalneedsapi.domain.entity")
-            
-            val properties = HashMap<String, Any?>().apply {
-                put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect")
-                put("hibernate.hbm2ddl.auto", "validate")
-                put("hibernate.show_sql", false)
-                put("hibernate.format_sql", false)
-                put("hibernate.connection.provider_disables_autocommit", true)
-                put("hibernate.cache.use_second_level_cache", false)
-                put("hibernate.cache.use_query_cache", false)
-                put("org.hibernate.envers.audit_table_suffix", "_history")
-                put("org.hibernate.envers.revision_field_name", "rev_id")
-                put("org.hibernate.envers.revision_type_field_name", "rev_type")
-                put("org.hibernate.envers.modified_flag_suffix", "_modified")
-                put("org.hibernate.envers.store_data_at_delete", true)
-                put("org.hibernate.envers.do_not_audit_optimistic_locking_field", false)
-            }
-            setJpaPropertyMap(properties)
-        }
+  @Bean(name = ["replicaEntityManagerFactory"])
+  fun replicaEntityManagerFactory(
+    @Qualifier("replicaDataSource") dataSource: DataSource,
+  ): LocalContainerEntityManagerFactoryBean = LocalContainerEntityManagerFactoryBean().apply {
+    setDataSource(dataSource)
+    setPersistenceProviderClass(HibernatePersistenceProvider::class.java)
+    persistenceUnitName = "replica"
+    setPackagesToScan("uk.gov.justice.digital.hmpps.supportadditionalneedsapi.domain.entity")
+
+    val properties = HashMap<String, Any?>().apply {
+      put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect")
+      put("hibernate.hbm2ddl.auto", "validate")
+      put("hibernate.show_sql", false)
+      put("hibernate.format_sql", false)
+      put("hibernate.connection.provider_disables_autocommit", true)
+      put("hibernate.cache.use_second_level_cache", false)
+      put("hibernate.cache.use_query_cache", false)
+      put("org.hibernate.envers.audit_table_suffix", "_history")
+      put("org.hibernate.envers.revision_field_name", "rev_id")
+      put("org.hibernate.envers.revision_type_field_name", "rev_type")
+      put("org.hibernate.envers.modified_flag_suffix", "_modified")
+      put("org.hibernate.envers.store_data_at_delete", true)
+      put("org.hibernate.envers.do_not_audit_optimistic_locking_field", false)
     }
+    setJpaPropertyMap(properties)
+  }
 
-    @Bean(name = ["replicaTransactionManager"])
-    fun replicaTransactionManager(
-        @Qualifier("replicaEntityManagerFactory") entityManagerFactory: LocalContainerEntityManagerFactoryBean
-    ): PlatformTransactionManager {
-        return JpaTransactionManager(entityManagerFactory.getObject()!!)
-    }
+  @Bean(name = ["replicaTransactionManager"])
+  fun replicaTransactionManager(
+    @Qualifier("replicaEntityManagerFactory") entityManagerFactory: LocalContainerEntityManagerFactoryBean,
+  ): PlatformTransactionManager = JpaTransactionManager(entityManagerFactory.getObject()!!)
 }
