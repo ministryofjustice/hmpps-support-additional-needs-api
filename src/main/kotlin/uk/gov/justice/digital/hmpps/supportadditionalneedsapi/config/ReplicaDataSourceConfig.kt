a7ff351db89832ac3c1c4e82ec4e4086328facf3
ayo.adedeji <ayo.adedeji@justice.gov.uk>
2025-11-03 11:09:27 +0000

    clean up

--- src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/config/ReplicaDataSourceConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/config/ReplicaDataSourceConfig.kt
@@ -42,27 +42,12 @@ class ReplicaDataSourceConfig {
     setPersistenceProviderClass(HibernatePersistenceProvider::class.java)
     persistenceUnitName = "replica"
     setPackagesToScan("uk.gov.justice.digital.hmpps.supportadditionalneedsapi.domain.entity")
-
-    val properties = HashMap<String, Any?>().apply {
-      put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect")
-      put("hibernate.hbm2ddl.auto", "validate")
-      put("hibernate.show_sql", false)
-      put("hibernate.format_sql", false)
-      put("hibernate.connection.provider_disables_autocommit", true)
-      put("hibernate.cache.use_second_level_cache", false)
-      put("hibernate.cache.use_query_cache", false)
-      put("org.hibernate.envers.audit_table_suffix", "_history")
-      put("org.hibernate.envers.revision_field_name", "rev_id")
-      put("org.hibernate.envers.revision_type_field_name", "rev_type")
-      put("org.hibernate.envers.modified_flag_suffix", "_modified")
-      put("org.hibernate.envers.store_data_at_delete", true)
-      put("org.hibernate.envers.do_not_audit_optimistic_locking_field", false)
-    }
-    setJpaPropertyMap(properties)
   }
 
   @Bean(name = ["replicaTransactionManager"])
   fun replicaTransactionManager(
     @Qualifier("replicaEntityManagerFactory") entityManagerFactory: LocalContainerEntityManagerFactoryBean,
-  ): PlatformTransactionManager = JpaTransactionManager(entityManagerFactory.getObject()!!)
+  ): PlatformTransactionManager = JpaTransactionManager(entityManagerFactory.getObject()!!).apply {
+    setJpaPropertyMap(mapOf("hibernate.connection.readOnly" to true))
+  }
 }
