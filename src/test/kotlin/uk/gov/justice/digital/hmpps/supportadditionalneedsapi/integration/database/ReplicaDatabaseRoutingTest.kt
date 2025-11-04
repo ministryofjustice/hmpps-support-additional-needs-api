1928216ad9d41d691d7589d2fe998d0e5a1fd04b
ayo.adedeji <ayo.adedeji@justice.gov.uk>
2025-11-04 12:01:44 +0000

    added test

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/integration/database/ReplicaDatabaseRoutingTest.kt
@@ -0,0 +1,43 @@
+package uk.gov.justice.digital.hmpps.supportadditionalneedsapi.integration.database
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import uk.gov.justice.digital.hmpps.supportadditionalneedsapi.domain.repository.reporting.EducationSupportPlansDueForCreationRepository
+import uk.gov.justice.digital.hmpps.supportadditionalneedsapi.integration.IntegrationTestBase
+import javax.sql.DataSource
+
+class ReplicaDatabaseRoutingTest : IntegrationTestBase() {
+    
+    @Autowired
+    @Qualifier("primaryDataSource")
+    private lateinit var primaryDataSource: DataSource
+    
+    @Autowired
+    @Qualifier("replicaDataSource")
+    private lateinit var replicaDataSource: DataSource
+    
+    @Autowired
+    private lateinit var educationSupportPlansDueForCreationRepository: EducationSupportPlansDueForCreationRepository
+
+    @Test
+    fun `reporting repository should be wired to replica entity manager`() {
+        // The repository should exist and be properly wired
+        assertThat(educationSupportPlansDueForCreationRepository).isNotNull
+
+        // The fact that this repository is in the reporting package and successfully
+        // injected means it's using the replica entity manager factory as configured
+        // in ReplicaDataSourceConfig
+    }
+    
+    @Test
+    fun `should have separate primary and replica datasources configured`() {
+        // Verify that we have two separate datasource beans
+        assertThat(primaryDataSource).isNotNull
+        assertThat(replicaDataSource).isNotNull
+        
+        // Verify they are different instances (different beans)
+        assertThat(primaryDataSource).isNotSameAs(replicaDataSource)
+    }
+}
