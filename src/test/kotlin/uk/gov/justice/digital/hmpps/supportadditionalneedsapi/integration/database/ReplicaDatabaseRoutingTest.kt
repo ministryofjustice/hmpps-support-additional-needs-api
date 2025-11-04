0fb4df575f256f283ee7073acf08ec7621631c1a
ayo.adedeji <ayo.adedeji@justice.gov.uk>
2025-11-04 12:05:15 +0000

    format

--- src/test/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/integration/database/ReplicaDatabaseRoutingTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/integration/database/ReplicaDatabaseRoutingTest.kt
@@ -9,35 +9,35 @@ import uk.gov.justice.digital.hmpps.supportadditionalneedsapi.integration.Integr
 import javax.sql.DataSource
 
 class ReplicaDatabaseRoutingTest : IntegrationTestBase() {
-    
-    @Autowired
-    @Qualifier("primaryDataSource")
-    private lateinit var primaryDataSource: DataSource
-    
-    @Autowired
-    @Qualifier("replicaDataSource")
-    private lateinit var replicaDataSource: DataSource
-    
-    @Autowired
-    private lateinit var educationSupportPlansDueForCreationRepository: EducationSupportPlansDueForCreationRepository
 
-    @Test
-    fun `reporting repository should be wired to replica entity manager`() {
-        // The repository should exist and be properly wired
-        assertThat(educationSupportPlansDueForCreationRepository).isNotNull
+  @Autowired
+  @Qualifier("primaryDataSource")
+  private lateinit var primaryDataSource: DataSource
 
-        // The fact that this repository is in the reporting package and successfully
-        // injected means it's using the replica entity manager factory as configured
-        // in ReplicaDataSourceConfig
-    }
-    
-    @Test
-    fun `should have separate primary and replica datasources configured`() {
-        // Verify that we have two separate datasource beans
-        assertThat(primaryDataSource).isNotNull
-        assertThat(replicaDataSource).isNotNull
-        
-        // Verify they are different instances (different beans)
-        assertThat(primaryDataSource).isNotSameAs(replicaDataSource)
-    }
+  @Autowired
+  @Qualifier("replicaDataSource")
+  private lateinit var replicaDataSource: DataSource
+
+  @Autowired
+  private lateinit var educationSupportPlansDueForCreationRepository: EducationSupportPlansDueForCreationRepository
+
+  @Test
+  fun `reporting repository should be wired to replica entity manager`() {
+    // The repository should exist and be properly wired
+    assertThat(educationSupportPlansDueForCreationRepository).isNotNull
+
+    // The fact that this repository is in the reporting package and successfully
+    // injected means it's using the replica entity manager factory as configured
+    // in ReplicaDataSourceConfig
+  }
+
+  @Test
+  fun `should have separate primary and replica datasources configured`() {
+    // Verify that we have two separate datasource beans
+    assertThat(primaryDataSource).isNotNull
+    assertThat(replicaDataSource).isNotNull
+
+    // Verify they are different instances (different beans)
+    assertThat(primaryDataSource).isNotSameAs(replicaDataSource)
+  }
 }
