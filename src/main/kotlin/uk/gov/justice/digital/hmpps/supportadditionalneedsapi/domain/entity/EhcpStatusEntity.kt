47610f1d5cfd32d1bfcc54460fdf5d7b34bbaafd
Nathan Russell <94835226+nathanrussell-moj-digital@users.noreply.github.com>
2026-04-16 17:39:19 +0100

    RR-2472 - Let JPA manage createdAt and updatedAt timestamps on entities (#383)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/domain/entity/EhcpStatusEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/supportadditionalneedsapi/domain/entity/EhcpStatusEntity.kt
@@ -3,17 +3,19 @@ package uk.gov.justice.digital.hmpps.supportadditionalneedsapi.domain.entity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EntityListeners
+import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 import org.hibernate.Hibernate
 import org.hibernate.annotations.CreationTimestamp
 import org.hibernate.annotations.UpdateTimestamp
+import org.hibernate.annotations.UuidGenerator
 import org.hibernate.envers.Audited
 import org.springframework.data.annotation.CreatedBy
 import org.springframework.data.annotation.LastModifiedBy
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.Instant
-import java.util.*
+import java.util.UUID
 
 @Entity
 @EntityListeners(value = [AuditingEntityListener::class])
@@ -23,6 +25,9 @@ data class EhcpStatusEntity(
   @Column(updatable = false)
   val prisonNumber: String,
 
+  @Column(updatable = false)
+  val reference: UUID = UUID.randomUUID(),
+
   @Column(nullable = false)
   var hasCurrentEhcp: Boolean = false,
 
@@ -31,30 +36,28 @@ data class EhcpStatusEntity(
 
   @Column
   var updatedAtPrison: String,
-
-  @Id
-  @Column
-  val id: UUID = UUID.randomUUID(),
-
-  @Column(updatable = false)
-  val reference: UUID = UUID.randomUUID(),
-
-  @CreatedBy
-  @Column(updatable = false)
-  var createdBy: String? = null,
-
-  @CreationTimestamp
-  @Column
-  var createdAt: Instant? = Instant.now(),
-
-  @LastModifiedBy
-  @Column
-  var updatedBy: String? = null,
-
-  @UpdateTimestamp
-  @Column
-  var updatedAt: Instant? = null,
 ) {
+  @Id
+  @GeneratedValue
+  @UuidGenerator
+  var id: UUID? = null
+
+  @Column(updatable = false)
+  @CreationTimestamp
+  var createdAt: Instant? = null
+
+  @Column(updatable = false)
+  @CreatedBy
+  var createdBy: String? = null
+
+  @Column
+  @UpdateTimestamp
+  var updatedAt: Instant? = null
+
+  @Column
+  @LastModifiedBy
+  var updatedBy: String? = null
+
   override fun equals(other: Any?): Boolean {
     if (this === other) return true
     if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
